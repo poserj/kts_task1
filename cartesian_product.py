0870578f8b7bb3c202cfd552af1f36aa4df641fa
PSN <ya.ry>
2023-01-31 15:04:10 +0800

    Декартово произведение

---
+++ cartesian_product.py
@@ -0,0 +1,21 @@
+from typing import Any
+import itertools
+
+__all__ = (
+    'cartesian_product',
+)
+
+
+def cartesian_product(arr1: list[Any], arr2: list[Any]) -> list:
+    """
+    Должна возвращать все пары элементы двух массивов:
+    cartesian_product([1, 2], [3, 4]) == [(1, 3), (1, 4), (2, 3), (2, 4)]
+    """
+    res = []
+    for i in itertools.product(arr1, arr2):
+        res.append(i)
+    return res
+
+
+if __name__ == '__main__':
+    print(cartesian_product([1, 2], [3, 4]))
