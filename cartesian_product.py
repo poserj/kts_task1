d9dacf8ff5c815e76afed8c670a244d364db9974
PSN <ya.ry>
2023-01-31 15:05:56 +0800

    Декартово произведение fix import

--- cartesian_product.py
+++ cartesian_product.py
@@ -1,5 +1,5 @@
 from typing import Any
-import itertools
+from itertools import product
 
 __all__ = (
     'cartesian_product',
@@ -12,7 +12,7 @@ def cartesian_product(arr1: list[Any], arr2: list[Any]) -> list:
     cartesian_product([1, 2], [3, 4]) == [(1, 3), (1, 4), (2, 3), (2, 4)]
     """
     res = []
-    for i in itertools.product(arr1, arr2):
+    for i in product(arr1, arr2):
         res.append(i)
     return res
 
