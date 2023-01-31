7a63e5454e404088e2f2fd22147ba3df5feaa630
PSN <ya.ry>
2023-01-31 20:12:38 +0800

    Переворачивание словаря

---
+++ kv_vk.py
@@ -0,0 +1,52 @@
+from typing import TypeVar
+
+__all__ = (
+    'flip_kv_vk',
+    'flip_kv_vk_safe',
+)
+
+
+KT = TypeVar('KT')
+KV = TypeVar('KV')
+
+
+def flip_kv_vk(d: dict[KT, KV]) -> dict[KV, KT]:
+    """
+    Функция должна возвращать словарь, в котором в качестве ключей - значения
+    переданного словаря, а в качестве значений - ключи.
+    Например,
+    flip_kv_vk({
+        'tokyo': 'Токио',
+        'moscow': 'Москва',
+    }) == {
+        'Токио': 'tokyo',
+        'Москва': 'moscow',
+    }
+    """
+    res = {}
+    for key, value in d.items():
+        res[value] = key
+    return res
+
+
+def flip_kv_vk_safe(d: dict[KT, KV]) -> dict[KV, list[KT]]:
+    """
+    Функция должна возвращать словарь, в котором в качестве ключей - значения
+    переданного словаря, а в качестве значений - массив ключей конфликтующих
+    значений.
+    Например,
+    flip_kv_vk({
+        'Санкт-Петербург': '+3',
+        'Москва': '+3',
+    }) == {
+        '+3': ['Москва', 'Санкт-Петербург'],
+    }
+    """
+    raise NotImplementedError
+
+
+if __name__ == '__main__':
+    print(flip_kv_vk({
+        'tokyo': 'Токио',
+        'moscow': 'Москва'
+    }))
